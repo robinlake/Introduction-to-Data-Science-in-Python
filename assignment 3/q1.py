34eed83cac73792a44ab85a89ae9950e66b3c1ee
Robin Lake <robinhlake@gmail.com>
2018-05-18 13:46:32 -0600

    finished q1

--- assignment 3/q1.py
+++ assignment 3/q1.py
@@ -1,21 +1,25 @@
 import numpy as np
 import pandas as pd
 
+###############
+# question one
+###############
+
 # load excel file
-df = pd.read_excel('Energy Indicators.xls')
+energy = pd.read_excel('Energy Indicators.xls')
 
 #drop header and footer
-df = df.loc[16:242, :]
+energy = energy.loc[16:242, :]
 
 # drop unnecessary columns
-df.drop(df.iloc[:, 0:2], axis=1, inplace=True)
+energy.drop(energy.iloc[:, 0:2], axis=1, inplace=True)
 
 # rename columns
 names = ['Country', 'Energy Supply', 'Energy Supply per Capita', '% Renewable']
-df.columns = names
+energy.columns = names
 
 # convert to gigajoules
-df['Energy Supply'] *= 1000000
+energy['Energy Supply'] *= 1000000
 
 # rename certain rows
 countries = {
@@ -24,13 +28,47 @@ countries = {
     "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
     "China, Hong Kong Special Administrative Region": "Hong Kong"
 }
+energy['Country'] = energy['Country'].replace(countries, regex=True)
 
 # remove numbers
-df['Country'] = df['Country'].str.replace('\d+', '')
+energy['Country'] = energy['Country'].str.replace('\d+', '')
 
 # remove text in parentheses
-df['Country'] = df['Country'].replace("\(.+\)", '', regex=True)
+energy['Country'] = energy['Country'].replace(" +\(.*\)", '', regex=True)
 
-df.rename(index=countries)
+# load gdp file
+GDP = pd.read_csv('world_bank.csv', skiprows=4)
 
-print(df.loc[230:, :])
+# rename countries
+countries2 = {
+    "Korea, Rep.": "South Korea",
+    "Iran, Islamic Rep.": "Iran",
+    "Hong Kong SAR, China": "Hong Kong"
+}
+GDP['Country Name'] = GDP['Country Name'].replace(countries2, regex=True)
+
+# load journal contribution file
+ScimEn = pd.read_excel('scimagojr-3.xlsx')
+
+# remove unused columns
+GDP = GDP[[
+    'Country Name', '2006', '2007', '2008', '2009', '2010', '2011', '2012',
+    '2013', '2014', '2015'
+]]
+
+# take top 15 rows
+ScimEn_m = ScimEn[:15]
+
+# merge dataframes
+merged = pd.merge(energy, GDP, left_on='Country', right_on='Country Name')
+merged1 = merged
+merged = pd.merge(ScimEn, merged, left_on='Country', right_on='Country')
+merged2 = merged
+# only keep top 15 countries
+merged = merged[:15]
+
+# set index to country name
+merged = merged.set_index('Country')
+
+# print(energy['Country'].sort_values()[80:150])
+print(merged)
